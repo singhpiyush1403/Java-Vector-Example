2631b659574b828b456a8aec31af6113c6392d21
PIYUSH SINGH <singhpiyush1403@gmail.com>
2025-03-29 23:14:22 +0530

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,13 @@
+import java.util.Vector;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> list = new Vector<>();
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        System.out.println(list);
+    }
+}
